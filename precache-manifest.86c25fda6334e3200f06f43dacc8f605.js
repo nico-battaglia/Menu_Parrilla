b13e48f4f74cd268db5c5e8fee3a72748cf5b6b9
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-22 04:22:32 -0300

    Updates

---
+++ precache-manifest.86c25fda6334e3200f06f43dacc8f605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "083e5ec51aa9a43fa70a",
+    "url": "/Menu_Parrilla/static/css/main.070be7b6.chunk.css"
+  },
+  {
+    "revision": "083e5ec51aa9a43fa70a",
+    "url": "/Menu_Parrilla/static/js/main.083e5ec5.chunk.js"
+  },
+  {
+    "revision": "756ceed5e136804fa3a6",
+    "url": "/Menu_Parrilla/static/js/1.756ceed5.chunk.js"
+  },
+  {
+    "revision": "41409250ae38b8e4f218",
+    "url": "/Menu_Parrilla/static/js/runtime~main.41409250.js"
+  },
+  {
+    "revision": "2d2a60fb4ac79db71173a8d20fb6daad",
+    "url": "/Menu_Parrilla/static/media/resto-bg.2d2a60fb.jpeg"
+  },
+  {
+    "revision": "afb9daf6a3b28e40e77f38774b02ddbf",
+    "url": "/Menu_Parrilla/index.html"
+  }
+];
