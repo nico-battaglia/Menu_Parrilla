4c91b5553a09ce4f2b91e7d753967e44771069de
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-05-16 23:02:05 -0300

    Add images locally

--- src/Menu.js
+++ src/Menu.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react"
 
-const Menu = ({items}) => {
-  
-  return <div className="section-center">
-  	
-	{items.map((item)=>{
-		  const {id, title, category, price, img, desc} = item;
-		  
-		  return <article key={id} className="menu-item">
-			  <img className="photo" src={img} alt={title}></img>
-			  <div className="item-info">
-				  <header>
-					  <h4>{title}</h4>
-					  <h4 className="price">${price}</h4>
-				  </header>
-				  <p className='item-text'> {desc}</p>
-			  </div>
-		  </article>
-	  }) }
-  </div>
-};
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((item) => {
+        const { id, title, category, price, img, desc } = item
 
-export default Menu;
+        return (
+          <article key={id} className="menu-item">
+            <img className="photo" src={img} alt={title}></img>
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text"> {desc}</p>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/img/images.js
@@ -0,0 +1,47 @@
+import bifeChorizo from "./00_bifeChorizo.jpg"
+import asado from "./01_asado.jpg"
+import vacio from "./02_vacio.jpg"
+import costillasCerdo from "./03_costillasCerdo.jpg"
+import bondiola from "./04_bondiola.jpg"
+import provoleta from "./05_provoleta.jpg"
+import papasFritas from "./06_papasFritas.jpg"
+import pure from "./08_pure.jpg"
+import ensalada from "./09_ensalada.webp"
+import huevosFritos from "./10_huevosFrito.webp"
+import bunuelos from "./11_buñuelos.jpg"
+import chinchu from "./12_chichulines.jpg"
+import mollejas from "./13_mollejas.jpg"
+import rinon from "./14_riñones.jpg"
+import pollo from "./15_pollo.jpg"
+import choriHuevo from "./16_choriHuevo.png"
+import matrimonio from "./17_matrimonio.jpg"
+import flan from "./18_flan.webp"
+import panquequeDulce from "./20_panqueque.jpg"
+import panquequeManzana from "./21_panqueque-de-manzana.jpg"
+import almendrado from "./22_almendrado.jpg"
+import helado from "./23_helado.webp"
+
+export {
+  bifeChorizo,
+  asado,
+  vacio,
+  costillasCerdo,
+  bondiola,
+  provoleta,
+  papasFritas,
+  pure,
+  ensalada,
+  huevosFritos,
+  bunuelos,
+  chinchu,
+  mollejas,
+  rinon,
+  pollo,
+  matrimonio,
+  panquequeDulce,
+  panquequeManzana,
+  flan,
+  almendrado,
+  helado,
+  choriHuevo,
+}
